56f998639f227f761b2d3b37bd7e78215026884f
Zeszito <liricractos@gmail.com>
2016-12-06 11:19:11 +0000

    Por O master certinho
    Isto é a base meu filhos

---
+++ GameBetSimulator/GameBetSimulator/Main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+
+//Funções a inicializar
+//Fim de Funções a inicializar
+
+//Main
+//Fim de main
+
+//Corpo das funções
+//Fim Corpo das FunÇões
+
+
