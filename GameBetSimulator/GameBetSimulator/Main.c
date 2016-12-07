4c0736f9599e95167f8affe94629e10aa2ac9c78
Moisés <liricractos@gmail.com>
2016-12-07 13:22:59 +0000

    EstrutraDefenida
    Tem as funções, sub funções e quem faz o quê.

--- GameBetSimulator/GameBetSimulator/Main.c
+++ GameBetSimulator/GameBetSimulator/Main.c
@@ -2,13 +2,31 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <time.h>
+#include <string.h>
+
 
 //Funções a inicializar
+float GeraCotas(); //O Miguel Vai dizer o que recebe 
+void GeraProbalbilidadesEquipa(); //Esqueleto miguel, todos juntos.
+void  PrintaMenu(); //O Moisés faz lindo menu, com asciiart.
+void  VerSaldo(); //O Moisés faz este. //Ver fluxograma //Escrevo ficheiro se nao houver.
+float AlteraSaldo(float valor);//Altera Saldo - Les ficheiro.
+							  //Se houver ficheiro - Le valor  e somas; 
+void  Jogar(); GeraCotas(); PrintResultado(); //AletraSaldo()
+void  Listar(); //Le o ficheiro e printa modalidade, jogos e resultados
+void  AlterarDefenicoes();
+void  Gravar();
+
 //Fim de Funções a inicializar
+
+//Structures
+
+//Fim structures
 
 //Main
 int main(void)
 {
+	printaMenu(); //AS funções do pdf
 
 }
 //Fim de main
@@ -16,5 +34,6 @@ int main(void)
 //Corpo das funções
 //Fim Corpo das FunÇões
 
-//Teste
+
+
 
