02465045e1956ee6d9830a93f8ea977a497f8a99
Moisés <liricractos@gmail.com>
2016-12-10 14:36:32 +0000

    Parte cima do menu
    Adiconei apenas o incio do menu <3 ficou fofo não?

--- GameBetSimulator/GameBetSimulator/Main.c
+++ GameBetSimulator/GameBetSimulator/Main.c
@@ -5,17 +5,18 @@
 #include <string.h>
 
 
-//Funções a inicializar
-float GeraCotas(); //O Miguel Vai dizer o que recebe 
-void GeraProbalbilidadesEquipa(); //Esqueleto miguel, todos juntos.
-void  PrintaMenu(); //O Moisés faz lindo menu, com asciiart.
-void  VerSaldo(); //O Moisés faz este. //Ver fluxograma //Escrevo ficheiro se nao houver.
-float AlteraSaldo(float valor);//Altera Saldo - Les ficheiro.
-							  //Se houver ficheiro - Le valor  e somas; 
-void  Jogar(); GeraCotas(); PrintResultado(); //AletraSaldo()
-void  Listar(); //Le o ficheiro e printa modalidade, jogos e resultados
-void  AlterarDefenicoes();
-void  Gravar();
+////Funções a inicializar
+void printaMenu();
+//float GeraCotas(); //O Miguel Vai dizer o que recebe 
+//void GeraProbalbilidadesEquipa(); //Esqueleto miguel, todos juntos.
+//void  PrintaMenu(); //O Moisés faz lindo menu, com asciiart.
+//void  VerSaldo(); //O Moisés faz este. //Ver fluxograma //Escrevo ficheiro se nao houver.
+//float AlteraSaldo(float valor);//Altera Saldo - Les ficheiro.
+//							  //Se houver ficheiro - Le valor  e somas; 
+//void  Jogar(); GeraCotas(); PrintResultado(); //AletraSaldo()
+//void  Listar(); //Le o ficheiro e printa modalidade, jogos e resultados
+//void  AlterarDefenicoes();
+//void  Gravar();
 
 //Fim de Funções a inicializar
 
@@ -27,11 +28,30 @@ void  Gravar();
 int main(void)
 {
 	printaMenu(); //AS funções do pdf
+	
 
 }
+
+
 //Fim de main
 
 //Corpo das funções
+void printaMenu() {
+	printf("						                     ___\ \n");
+	printf("						 o__        o__     |   |\\\n");
+	printf("						/|          /\\      |   |X\\\n");
+	printf("						/ > o        <\\     |   |XX\\\n");
+	printf("		 ______    _        _____  _                         _ \n");
+	printf("		|  ____|  | |      |  __ \\| |                       | |\n");
+	printf("		| |__ __ _| | _____| |__) | | __ _  ___ __ _ _ __ __| |\n");
+	printf("		|  __/ _` | |/ / _ \\  ___/| |/ _` |/ __/ _` | '__/ _` |\n");
+	printf("		| | | (_| |   <  __/ |    | | (_| | (_| (_| | | | (_| |\n");
+	printf("		|_|  \\__,_|_|\\_\\___|_|    |_|\\__,_|\\___\\__,_|_|  \\__,_|\n");
+	printf("								By:Andre, Miguel e Moises\n");
+	printf("\n");
+
+
+}
 //Fim Corpo das FunÇões
 
 
