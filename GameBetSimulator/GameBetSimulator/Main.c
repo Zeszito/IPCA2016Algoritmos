9de481a9b6a2255c574f2912ff4606a8e3a09510
Moisés <liricractos@gmail.com>
2016-12-13 12:24:59 +0000

    MenuQuseCompleto
    Adiocionei a funçao menu, a escolha, preveni o erro apenas falta executar as funçoes.

--- GameBetSimulator/GameBetSimulator/Main.c
+++ GameBetSimulator/GameBetSimulator/Main.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -5,8 +6,11 @@
 #include <string.h>
 
 
+
 ////Funções a inicializar
-void printaMenu();
+void GeraCabecalho();
+void GeraMenu();
+void ExecutaEscolha();
 //float GeraCotas(); //O Miguel Vai dizer o que recebe 
 //void GeraProbalbilidadesEquipa(); //Esqueleto miguel, todos juntos.
 //void  PrintaMenu(); //O Moisés faz lindo menu, com asciiart.
@@ -27,16 +31,27 @@ void printaMenu();
 //Main
 int main(void)
 {
-	printaMenu(); //AS funções do pdf
+	char chose;
+	GeraCabecalho(); //Printa o topo
+	GeraMenu();	
+	//Escolhas
+	scanf("%c", &chose);
+	getchar(); //Para já serve	
+	while (!(chose == '1' || chose == '2' || chose == '3' || chose == '4' || chose == '5' || chose == '6'))
+	{
+		printf("escolha incorrecta, escolha novamente\n");
+		scanf("%c", &chose);
+		getchar();
+	}
 	
-
+    
 }
 
 
 //Fim de main
 
 //Corpo das funções
-void printaMenu() {
+void GeraCabecalho() {
 	printf("						                     ___\ \n");
 	printf("						 o__        o__     |   |\\\n");
 	printf("						/|          /\\      |   |X\\\n");
@@ -49,9 +64,142 @@ void printaMenu() {
 	printf("		|_|  \\__,_|_|\\_\\___|_|    |_|\\__,_|\\___\\__,_|_|  \\__,_|\n");
 	printf("								By:Andre, Miguel e Moises\n");
 	printf("\n");
-
-
+    printf("\n");
 }
+//FUNÇOES DE MENU
+//Auxiliares
+void printaTopo() {
+	int i;
+	printf("%c", 201);// cantoSupEsquerdo = 201;
+	for (i = 0; i < 35; i++) { printf("%c", 205); }//linharecta = 221;  //35 primeiro bloco 
+	printf("%c", 187);
+	//segunda linha de cima
+	printf("%c", 201);// cantoSupEsquerdo = 201;
+	for (i = 0; i < 12; i++) { printf("%c", 205); }//linharecta = 221;  //35 primeiro bloco 
+	printf("%c", 187);
+	printf("\n");
+}
+void printaMeio(){
+	int i;
+	printf("%c", 204);// uniao direita
+	for (i = 0; i < 35; i++) { printf("%c", 205); }//linharecta = 221;  //35 primeiro bloco 
+	printf("%c", 185); //uniao esquerda
+	//segunda linha de cima
+	printf("%c", 204);// uniao direita
+	for (i = 0; i < 12; i++) { printf("%c", 205); }//linharecta = 221;  //35 primeiro bloco 
+	printf("%c", 185);// uniao esuqerda
+	printf("\n");
+}
+void printaParteDeBaixo() {
+	int i;
+	printf("		");
+	printf("%c", 200);// cantoInfEsquerdo ;
+	for (i = 0; i < 35; i++) { printf("%c", 205); }//linharecta = 221;  //35 primeiro bloco 
+	printf("%c", 188);
+	//segunda linha de cima
+	printf("%c", 200);// cantoinfEsquerdo ;
+	for (i = 0; i < 12; i++) { printf("%c", 205); }//linharecta = 221;  //35 primeiro bloco 
+	printf("%c", 188);
+	printf("\n");
+}
+void printaMeioNoLines() {
+	int i;
+	printf("		");
+	printf("%c", 186);// barra continua
+	for (i = 0; i < 35; i++) { printf("%c", 32); }//Espaço
+	printf("%c", 186); // barra continua
+	printf("%c", 186);// barra continua
+	for (i = 0; i < 12; i++) { printf("%c", 32); }//Espaço
+	printf("%c", 186);// barra continua
+	printf("\n");
+}
+//PrincipaL
+void GeraMenu() { 
+	//Desenhar tabela//	
+	printf("		");
+	printaTopo();
+	int i = 0;	
+	//Recheio
+	printf("		");
+	printf("%c", 186);
+	for (i = 0; i < 12; i++) { printf("%c", 32); }
+	printf("MENU DE JOGO");
+	for (i = 0; i < 11; i++) { printf("%c", 32); }
+	printf("%c%c", 186, 186);
+	printf("   Opcoes   ");
+	printf("%c", 186);
+	printf("\n");
+	//escolhaS;
+	printf("		");
+	printaMeio();
+	//Recheio1
+	printf("		");
+	printf("%c", 186);
+	for (i = 0; i < 12; i++) { printf("%c", 32); }
+	printf("Ver Saldo");
+	for (i = 0; i < 14; i++) { printf("%c", 32); }
+	printf("%c%c", 186, 186);
+	printf("     1      ");
+	printf("%c", 186);
+	printf("\n");
+	printaMeioNoLines();
+	//Recheio2
+	printf("		");
+	printf("%c", 186);
+	for (i = 0; i < 12; i++) { printf("%c", 32); }
+	printf("Jogar");
+	for (i = 0; i < 18; i++) { printf("%c", 32); }
+	printf("%c%c", 186, 186);
+	printf("     2      ");
+	printf("%c", 186);
+	printf("\n");
+	printaMeioNoLines();
+	//Recheio3
+	printf("		");
+	printf("%c", 186);
+	for (i = 0; i < 12; i++) { printf("%c", 32); }
+	printf("Listar");
+	for (i = 0; i < 17; i++) { printf("%c", 32); }
+	printf("%c%c", 186, 186);
+	printf("     3      ");
+	printf("%c", 186);
+	printf("\n");
+	printaMeioNoLines();
+	//Recheio4
+	printf("		");
+	printf("%c", 186);
+	for (i = 0; i < 12; i++) { printf("%c", 32); }
+	printf("Alterar definicoes");
+	for (i = 0; i < 5; i++) { printf("%c", 32); }
+	printf("%c%c", 186, 186);
+	printf("     4      ");
+	printf("%c", 186);
+	printf("\n");
+	printaMeioNoLines();
+	//Recheio5
+	printf("		");
+	printf("%c", 186);
+	for (i = 0; i < 12; i++) { printf("%c", 32); }
+	printf("Gravar");
+	for (i = 0; i < 17; i++) { printf("%c", 32); }
+	printf("%c%c", 186, 186);
+	printf("     5      ");
+	printf("%c", 186);
+	printf("\n");
+	printaMeioNoLines();
+	//Recheio6
+	printf("		");
+	printf("%c", 186);
+	for (i = 0; i < 12; i++) { printf("%c", 32); }
+	printf("Sair");
+	for (i = 0; i < 19; i++) { printf("%c", 32); }
+	printf("%c%c", 186, 186);
+	printf("     6      ");
+	printf("%c", 186);
+	printf("\n");
+	printaParteDeBaixo();
+	}
+//Fim Funcoes de Menu
 //Fim Corpo das FunÇões
 
 
